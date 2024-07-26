9991258a936d1fb2044d4c4f0d10130e823595ba
Gimachombe <jacobgimachombe@gmail.com>
2024-07-26 14:28:48 +0300

    remove white spaces add env file to gitignore

--- sqlAlchemy_Practice/create_tables.py
+++ sqlAlchemy_Practice/create_tables.py
@@ -3,4 +3,3 @@ from models import User, Post
 
 # Create the table in the database
 Base.metadata.create_all(engine)
-
--- sqlAlchemy_Practice/database.py
+++ sqlAlchemy_Practice/database.py
@@ -1,4 +1,4 @@
-import os 
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -6,21 +6,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-#step 1: create engine
-#create_engine establishes a connection to the database
+# step 1: create engine
+# create_engine establishes a connection to the database
 engine = create_engine(DATABASE_URL, echo=True)
 
-#step 2: create our declarative base
+# step 2: create our declarative base
 # declarative_base function is used to create a base class for defining ORM models
 Base = declarative_base()
 
-#step 3: create your session
-#Session is created to interact with the database. It manages transactions and keeps track of changes to objects.
+# step 3: create your session
+# Session is created to interact with the database. It manages transactions and keeps track of changes to objects.
 
 
 Session = sessionmaker(bind=engine)
 
-session=Session()
-
+session = Session()
